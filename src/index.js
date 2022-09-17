87d89330a3e61f1a3a88fe4b4a9e47ed758b5353
Gustavo backes <kitzgustavo@gmail.com>
2022-09-16 22:29:44 -0300

    feito ajustes no botao

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 function aviso() {
   titulo.style.cssText = "visibility: visible";
+  titulo2.style.cssText = "visibility: visible";
 }
+
 const titulo = document.querySelector("#aviso");
 titulo.addEventListener("click", aviso);
+
+const titulo2 = document.querySelector("#easterEgg");
+titulo2.addEventListener("click", aviso);
